a011ec675148f14c68289d28e8f848b815525b29
nbansal16 <36689548+nbansal16@users.noreply.github.com>
2018-03-06 12:34:48 +0530

    Create views.groovy

---
+++ docker/master/init.color.d/views.groovy
@@ -0,0 +1,22 @@
+def defaultColumns = {
+        status()
+        weather()
+        name()
+        lastSuccess()
+        lastFailure()
+        lastDuration()
+        slaveOrLabel()
+        configureProject()
+        buildButton()
+        disableProject(true)
+}
+
+
+listView('SAMPLE_VIEW') {
+    filterBuildQueue()
+    filterExecutors()
+    jobs {
+        regex(/^sample_.*/)
+    }
+    columns defaultColumns
+}
