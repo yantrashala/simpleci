9a8f8a8d2ec7aa78ec2d73427bac386b0168247a
nbansal16 <36689548+nbansal16@users.noreply.github.com>
2018-03-12 16:54:56 +0530

    The URL of github updated to yantrashala

--- init.groovy.d/config-init.groovy
+++ init.groovy.d/config-init.groovy
@@ -9,29 +9,29 @@ import com.cloudbees.plugins.credentials.SystemCredentialsProvider;
 import com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey;
 import com.cloudbees.plugins.credentials.CredentialsScope;
 
-	def inst = Jenkins.getInstance()
-	def desc = inst.getDescriptor("org.jenkinsci.plugins.workflow.libs.GlobalLibraries")
+def inst = Jenkins.getInstance()
+def desc = inst.getDescriptor("org.jenkinsci.plugins.workflow.libs.GlobalLibraries")
 
-  // 	Jenkins SSH Credentials
+// 	Jenkins SSH Credentials
 	def system_credentials_provider = SystemCredentialsProvider.getInstance()	
 	def ssh_key_scope = CredentialsScope.GLOBAL
-  def ssh_key_id = "jenkins-master"
-  def ssh_key_username = "jenkins"
-  def ssh_key_private_key_source = new BasicSSHUserPrivateKey.UsersPrivateKeySource()
-  def ssh_key_passphrase = null
+    	def ssh_key_id = "jenkins-master"
+    	def ssh_key_username = "jenkins"
+    	def ssh_key_private_key_source = new BasicSSHUserPrivateKey.UsersPrivateKeySource()
+   	def ssh_key_passphrase = null
  	def ssh_key_description = "Jenkins Master"
-  def ssh_key_domain = com.cloudbees.plugins.credentials.domains.Domain.global()
-  def ssh_key_creds = new BasicSSHUserPrivateKey(ssh_key_scope,ssh_key_id,ssh_key_username,ssh_key_private_key_source,ssh_key_passphrase,ssh_key_description)
+    	def ssh_key_domain = com.cloudbees.plugins.credentials.domains.Domain.global()
+    	def ssh_key_creds = new BasicSSHUserPrivateKey(ssh_key_scope,ssh_key_id,ssh_key_username,ssh_key_private_key_source,ssh_key_passphrase,ssh_key_description)
 
-  system_credentials_provider.addCredentials(ssh_key_domain,ssh_key_creds)
+    	system_credentials_provider.addCredentials(ssh_key_domain,ssh_key_creds)
 	// this is for Legacy SCM Retrieval Method
 	// SCM scm = new GitSCM("https://git.example.com/foo.git")
 	// SCMRetriever retriever = new SCMRetriever(scm)
 	
 	// This is for Modern SCM as Retrieval Method
 	SCMSourceRetriever retriever = new SCMSourceRetriever(new GitSCMSource(
-        "someId",
-        "https://github.com/CodeValet/master.git",
+        "scmSourceId",
+        "https://github.com/yantrashala/simpleci-shared-library.git",
         "jenkins-master",
         "*",
         "",
@@ -43,3 +43,7 @@ import com.cloudbees.plugins.credentials.CredentialsScope;
 	libconfig.setImplicit(true)
 	libconfig.setAllowVersionOverride(false)
 	desc.get().setLibraries([libconfig])
+
+
+
+
