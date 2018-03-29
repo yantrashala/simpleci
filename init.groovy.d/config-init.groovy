19fa8e7dc9a7c3fab8c7147dc2c4ee8ad66628f7
nbansal16 <36689548+nbansal16@users.noreply.github.com>
2018-03-29 13:12:29 +0530

    Update config-init.groovy

--- init.groovy.d/config-init.groovy
+++ init.groovy.d/config-init.groovy
@@ -10,31 +10,35 @@ import com.cloudbees.plugins.credentials.CredentialsScope;
 import com.cloudbees.plugins.credentials.impl.*;
 import com.cloudbees.plugins.credentials.*;
 import com.cloudbees.plugins.credentials.domains.*;
+import java.util.Base64;
 
 	// Fetch values from Environment Variables
 	def shared_library_url 		= System.getenv("SHARED_LIBRARY_URL")
 	def shared_library_version 	= System.getenv("SHARED_LIBRARY_VERSION")
 	def shared_library_name 	= System.getenv("SHARED_LIBRARY_NAME")
-	def inst 			= Jenkins.getInstance()
-	def desc 			= inst.getDescriptor("org.jenkinsci.plugins.workflow.libs.GlobalLibraries")
-	def home_dir 			= System.getenv("JENKINS_CONF")
-	def properties 			= new ConfigSlurper().parse(new File("$home_dir/simpleci.conf").toURI().toURL())
-
+	def inst 					= Jenkins.getInstance()
+	def desc 					= inst.getDescriptor("org.jenkinsci.plugins.workflow.libs.GlobalLibraries")
+	def home_dir 				= System.getenv("JENKINS_CONF")
+	def properties 				= new ConfigSlurper().parse(new File("$home_dir/simpleci.conf").toURI().toURL())
+	
 	def descript
-
+	
 	// Jenkins Credentials
 	properties.credentials.each() { configName, serverConfig ->
+	
+		// Decode password using base64
+		byte[] valueDecoded =  Base64.getDecoder().decode(serverConfig.password);
 		Credentials creds = (Credentials) new UsernamePasswordCredentialsImpl(CredentialsScope.GLOBAL,
 													  serverConfig.credentialsId,
 													  serverConfig.description,
-													  serverConfig.username,serverConfig.password
+													  serverConfig.username,new String(valueDecoded)
 													  /*new File(serverConfig.path).text.trim()*/)
 		descript = serverConfig.credentialsId;
 		SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), creds)
 	}
 
 	if(shared_library_url!=null && shared_library_version!=null && shared_library_name!=null ){
-
+		
 		//This is for Modern SCM as Retrieval Method
 		SCMSourceRetriever retriever = new SCMSourceRetriever(new GitSCMSource(
 			"scmSourceId",
@@ -43,7 +47,7 @@ import com.cloudbees.plugins.credentials.domains.*;
 			"*",
 			"",
 			false))
-
+		
 		def name = shared_library_name  
 		LibraryConfiguration libconfig = new LibraryConfiguration(name, retriever)
 		libconfig.setDefaultVersion(shared_library_version)
@@ -53,7 +57,7 @@ import com.cloudbees.plugins.credentials.domains.*;
 	}
 	//In absence of env_var, look into property file for configurations
 	else{
-
+	
 		properties.sharedLibrary.each() { configName, serverConfig ->
 			// This is for Modern SCM as Retrieval Method
 			SCMSourceRetriever retriever = new SCMSourceRetriever(new GitSCMSource(
@@ -63,7 +67,7 @@ import com.cloudbees.plugins.credentials.domains.*;
 				"*",
 				"",
 				false))
-
+			
 			def name = serverConfig.libraryName  
 			LibraryConfiguration libconfig = new LibraryConfiguration(name, retriever)
 			libconfig.setDefaultVersion(serverConfig.version)
